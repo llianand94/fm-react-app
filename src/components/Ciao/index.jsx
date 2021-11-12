db13eae7c57890081246208b0583687e7e2fcc2e
Andrey Shapovalov <llianandr94@gmai.com>
2021-11-12 12:04:25 +0200

    Blank before task

--- src/components/Ciao/index.jsx
+++ src/components/Ciao/index.jsx
@@ -1,36 +1,27 @@
-import React, {Component} from "react";
-
-class Ciao extends Component{  
+import {Component} from 'react';
+class Ciao extends Component{
   constructor(props){
     super(props);
     this.state = {
       isHi:true
-    };
+    }
   }
-  hanlerButton=()=>{
-    const {isHi} = this.state;   
+  handlerBtn = ()=>{
+    const {isHi} = this.state;
     this.setState({
-      isHi:!isHi  
+      isHi:!isHi
     })
-}
+  }
   render(){
-    console.log(this.state.isHi);
     const {name} = this.props;
     const {isHi} = this.state;
-   
     if(isHi){
       return <>
-      <h2> Hello {name}!</h2>
-      <button onClick={this.hanlerButton}> Ciao </button>
-      </>;
+        <h2>Hi, {name}!</h2>
+        <button onClick={this.handlerBtn}>Ciao</button>
+        </>;
     }
-    return <>
-      <h2> Bye {name}!</h2>
-       </>;
-    
-        
-    
+    return <h2>Bye, {name}!</h2>;
   }
 }
-
 export default Ciao;
