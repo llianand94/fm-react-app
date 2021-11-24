2dee394005b6df3902078ca3e7e482075784ddb4
Andrey Shapovalov <llianandr94@gmai.com>
2021-11-24 12:01:27 +0200

    Add select be nat

--- src/components/UserLoader/index.js
+++ src/components/UserLoader/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import { getUsers } from '../../api';
 import Spinner from './Spinner';
 import config from '../../config';
@@ -14,12 +13,13 @@ class UsersLoader extends Component {
       isError: false,
       currentPage: 1,
       currentResults:config.DEFAULT_AMOUNT,
+      currentNat:'gb'
     }
   }
 
   load = () => {
-    const {currentPage:page, currentResults:results} = this.state;
-    getUsers({page,results})
+    const {currentPage:page, currentResults:results, currentNat:nat} = this.state;
+    getUsers({page,results, nat})
       .then((data)=>{
       if(data.error){
         return this.setState({
@@ -45,8 +45,10 @@ class UsersLoader extends Component {
     this.load();
   }
   componentDidUpdate(prevProps, prevState){
-    const {currentPage,currentResults} = this.state;
-    if(currentPage!==prevState.currentPage||currentResults!==prevState.currentResults){
+    const {currentPage,currentResults, currentNat} = this.state;
+    const isUpdate = currentPage!==prevState.currentPage||
+          currentResults!==prevState.currentResults||currentNat!==prevState.currentNat;
+    if(isUpdate){
       this.load();
     }    
   }
@@ -60,7 +62,7 @@ class UsersLoader extends Component {
   }
   nextPage = () => this.setState((state,props)=>({currentPage:state.currentPage+1}));
 
-  createUser= (user) => <li key={user.login.uuid}>{JSON.stringify(user.name,null, 7)}</li>
+  createUser= (user) => <li key={user.login.uuid}>{JSON.stringify(user.name,null, 7)} {user.nat}</li>
     
   radioHandler = (e) =>{       
     this.setState({currentResults: e.target.value})     
@@ -70,8 +72,11 @@ class UsersLoader extends Component {
       return true
     }    
   }
+  selectHandler = (e) =>{
+    this.setState({currentNat: e.target.value})
+  }
   render() {
-    const {users, isFetching, isError, currentPage, currentResults} = this.state;
+    const {users, isFetching, isError, currentPage, currrentNat} = this.state;
     // if(isError){
     //   return <div>Error</div>;
     // }
@@ -85,9 +90,15 @@ class UsersLoader extends Component {
       <button onClick={this.prevPage}>&lt;</button>
       <button onClick={this.nextPage}>&gt;</button>      
       <p>current page: {currentPage}</p>
+      <select value={currrentNat} onChange={this.selectHandler}>
+        <option value="au">AU</option>
+        <option value="br">BR</option>
+        <option value="fr">FR</option>
+        <option value="gb">GB</option>
+      </select>
       <div>
-        <label><input type="radio" name="results" checked={this.checkChecked(5)} value={5} onClick={this.radioHandler}/>5</label>
-        <label><input type="radio" name="results" value={10} onChange={this.radioHandler} checked={this.checkChecked(10)}/ >10</label>
+        <label><input type="radio" name="results" checked={this.checkChecked(5)} value={5} onChange={this.radioHandler}/>5</label>
+        <label><input type="radio" name="results" value={10} onChange={this.radioHandler} checked={this.checkChecked(10)}/>10</label>
         <label><input type="radio" name="results" value={15} onChange={this.radioHandler} checked={this.checkChecked(15)}/>15</label>
       </div>
       <ul>
@@ -98,8 +109,6 @@ class UsersLoader extends Component {
   }
 }
 
-
-UsersLoader.propTypes = {};
 
 
 export default UsersLoader;
