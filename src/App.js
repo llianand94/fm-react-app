186d13958dc696adf0f95abd803e2698107416dd
Andrey Shapovalov <llianandr94@gmai.com>
2021-11-25 10:54:32 +0200

    Add window.innerWidth exsample be event 'resize'

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@
 import './App.css';
 import React from 'react';
 import UserLoader from './components/UserLoader';
+import WindowSizes from './components/WindowSizes';
 
 
 
 
 
 function App(){
-  return <>   
-    <UserLoader/>
+  return <>  
+     <WindowSizes/>
+    {/* <button onClick={()=>{window.screen()}}>Close Window</button>
+    <button onClick={()=>{window.location.assign()}}>Close Window</button> */}
  </>
 }   
   
