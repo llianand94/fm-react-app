122cc596227f86e27d5b120b210dafa27c32c9de
Andrey Shapovalov <llianandr94@gmai.com>
2021-11-25 11:34:41 +0200

    Add example use custom components react-route-dom

--- src/App.js
+++ src/App.js
@@ -3,17 +3,39 @@ import './App.css';
 import React from 'react';
 import UserLoader from './components/UserLoader';
 import WindowSizes from './components/WindowSizes';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 
-
-
-function App(){
-  return <>  
-     <WindowSizes/>
-    {/* <button onClick={()=>{window.screen()}}>Close Window</button>
-    <button onClick={()=>{window.location.assign()}}>Close Window</button> */}
- </>
+const Home = () =>{ return <h1>Home</h1>}
+const About = () =>{ return <h1>About</h1>}
+const Contact = () =>{ return <h1>Contact</h1>}
+function App(){  
+  return <BrowserRouter>  
+  <nav>
+    <ul>
+      <li>
+        <Link to='/'>Home</Link>
+      </li>
+      <li>
+        <Link to='/about'></Link>
+      </li>
+      <li>
+        <Link to='/contact'></Link>
+      </li>
+    </ul>
+  </nav>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>   
+      <Route path="/about" element={<About />}></Route>   
+      <Route path="/contact" element={<Contact />}></Route>   
+    </Routes>    
+ </BrowserRouter>
 }   
   
 export default App;
